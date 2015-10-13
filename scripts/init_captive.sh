6967fe9b759832e9cb9c89a68b78f1988465df55
Martin Brugnara <martin.brugnara@gmail.com>
2015-10-13 16:05:03 +0200

    fix typo
    Fix typo and minor style things

--- scripts/init_captive.sh
+++ scripts/init_captive.sh
@@ -21,11 +21,11 @@ iptables -P FORWARD ALLOW
 
 # enable captive portal website
 iptables -A INPUT -i $IN_DEV -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -o $IN_DEV -p tcp --dport 80 -m state --state ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o $IN_DEV -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
 
 # enable nat
 echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -A POSTROUTING -t nat -o eth0 -j MASQUERADE
+iptables -A POSTROUTING -t nat -o $GW_DEV -j MASQUERADE
 
 # captive portal filter
 iptables -t mangle -N internet 
@@ -44,8 +44,8 @@ iptables -I FORWARD -p udp -i $IN_DEV -m multiport --sports 53,67,123 -j ACCEPT
 # ------------------------------------------------------------------------------
 # User management:
 # allow
-iptables -t mangle -I internet 1 -m mac --mac-source USER-MAC-ADDRESS-HERE -s USER-IP -j RETURN
+iptables -t mangle -I internet 1 -m mac --mac-source $USER_MAC_ADDRESS -s $USER_IP -j RETURN
 
 # block
-iptables -t mangle -D internet  -m mac --mac-source USER_MAC_ADDRESS -s USER-IP -j RETURN
+iptables -t mangle -D internet  -m mac --mac-source $USER_MAC_ADDRESS -s $USER_IP -j RETURN
 
